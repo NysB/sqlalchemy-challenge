1bce9e77949050944f33cda067d95f0a1d76ba38
NysB <123317735+NysB@users.noreply.github.com>
2023-04-30 17:04:11 +0100

    Final Commit

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -30,6 +30,46 @@ station = base.classes.station
 
 app = Flask(__name__)
 
+# Start session
+session = Session(engine)
+
+# Find most recent date 
+date_list = session.query(measurement.date)
+recent_date = [x[0] for x in date_list]
+recent_date = max(recent_date)
+    
+# Find date 12 months before
+recent_date_clean = dt.datetime.strptime(recent_date, "%Y-%m-%d").date()
+date_one_year = recent_date_clean - dt.timedelta(days=365)
+
+# Retrieve station names
+station_list = session.query(measurement.station)
+
+station_names = []
+    
+for name in station_list:
+    station_names.append(name)
+
+station_names = set(station_names)
+station_names = [x[0] for x in station_names]
+
+# Find most active station
+station_counts = []
+
+for station in station_names:
+    number_rows = session.query(measurement.station).filter(measurement.station == station).count()
+    station_counts.append(number_rows)
+    
+active_stations_df = pd.DataFrame({"station": station_names,
+                                   "number of rows": station_counts})
+    
+active_stations_df = active_stations_df.sort_values("number of rows",ascending=False)
+active_stations_df = active_stations_df.reset_index(drop=True)
+
+# Close session
+session.close()
+
+
 #################################################
 # Flask Routes
 #################################################
@@ -41,22 +81,13 @@ def homepage():
         "/Resources/hawaii.sqlite <br/>"
         "/api/v1.0/stations <br/>"
         "/api/v1.0/tobs <br/>"
-        "/api/v1.0/<start> <br/>"
-        "/api/v1.0/<start>/<end>")
+        "/api/v1.0/start-date (yyyy-mm-dd) <br/>"
+        "/api/v1.0/start-date (yyyy-mm-dd)/end-date (yyyy-mm-dd)")
 
 @app.route("/Resources/hawaii.sqlite")
 def hawaii():
     # Start session
     session = Session(engine)
-
-    # Find most recent date 
-    date_list = session.query(measurement.date)
-    recent_date = [x[0] for x in date_list]
-    recent_date = max(recent_date)
-    
-    # Find date 12 months before
-    recent_date_clean = dt.datetime.strptime(recent_date, "%Y-%m-%d").date()
-    date_one_year = recent_date_clean - dt.timedelta(days=365)
 
     # Query DB
     one_year_prec = session.query(measurement).filter(measurement.date >= date_one_year).filter(measurement.date <= recent_date_clean)
@@ -68,63 +99,90 @@ def hawaii():
     
     # Jsonify
     return jsonify(prpc_dict )
-
+    
     # Close session
     session.close()
+
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # Start session
-    session = Session(engine)
-
-    # Query DB to retrieve station names
-    station_list = session.query(measurement.station)
-
-    # Add names to list
-    station_names = []
-    
-    for name in station_list:
-        station_names.append(name)
-    
-    station_names = set(station_names)
-    station_names = [x[0] for x in station_names]
-
     # Jsonify
     return jsonify(station_names)
     
-    # Close session
-    session.close()
-
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     # Start session
     session = Session(engine)
-
-    # Find most active station
-    station_counts = []
     
-    for station in station_names:
-        number_rows = session.query(measurement.station).filter(measurement.station == station).count()
-        station_counts.append(number_rows)
-    
-    active_stations_df = pd.DataFrame({"station": station_names,
-                                   "number of rows": station_counts})
-    
-    active_stations_df = active_stations_df.sort_values("number of rows",ascending=False)
-    active_stations_df = active_stations_df.reset_index(drop=True)
-      
     # Query DB to retrieve last 12 months data
-    last_12_months = session.query(measurement.tobs).filter(measurement.station == active_stations_df["station"][0]).filter(measurement.date >= date_one_year).filter(measurement.date <= recent_date_clean)
+    last_12_months_tobs = session.query(measurement).filter(measurement.station == active_stations_df["station"][0]).filter(measurement.date >= date_one_year).filter(measurement.date <= recent_date_clean)
+
+    # Save query results
     
-    temp_list = [x[0] for x in last_12_months]
+    last_12_months = [{"date": x.date,
+                  "tobs": x.tobs} for x in last_12_months_tobs]
 
     # Jsonify
-    return jsonify(temp_list)
+    return jsonify(last_12_months)
     
     # Close session
     session.close()
 
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    # Start session
+    session = Session(engine)
+
+    # Query DB 
+    temp_data = session.query(measurement.tobs).filter(measurement.date >= str(start))
+
+    # Save query results
+    temp_data_list = [x[0] for x in temp_data]
+
+    # Calculate
+    max_temp = max(temp_data_list)
+    min_temp = min(temp_data_list)
+    mean_temp = statistics.mean(temp_data_list)
+    
+    # Summarize data
+    temp_data_dict = {"lowest value": min_temp,
+                      "highest value": max_temp,
+                      "average": mean_temp}
+    
+    # Jsonify
+    return jsonify(temp_data_dict)
+
+    # Close session
+    session.close()
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def end_date(start,end):
+    # Start session
+    session = Session(engine)
+
+    # Query DB 
+    temp_data = session.query(measurement.tobs).filter(measurement.date >= str(start)).filter(measurement.date <= str(end))
+
+    # Save query results
+    temp_data_list = [x[0] for x in temp_data]
+
+    # Calculate
+    max_temp = max(temp_data_list)
+    min_temp = min(temp_data_list)
+    mean_temp = statistics.mean(temp_data_list)
+    
+    # Summarize data
+    temp_data_dict = {"lowest value": min_temp,
+                      "highest value": max_temp,
+                      "average": mean_temp}
+    
+    # Jsonify
+    return jsonify(temp_data_dict)
+
+    # Close session
+    session.close()
 
 
 if __name__ == "__main__":
